64c795851324a2c554aeb7defb7053ef70434f51
DarthChurros <tnarayan00@stuy.edu>
2019-12-05 11:15:47 -0500

    wrote imports

---
+++ smash.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <dirent.h>
